c932f346d356c2bb0e7f45a3698092dfdd7b0f34
xdedzl <32391533+xdedzl@users.noreply.github.com>
2019-01-11 17:40:10 +0800

    Loom

---
+++ Assets/Scripts/Thread/LoomTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class LoomTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Loom.Initialize();
+
+        DoLoomThread();
+    }
+
+    void DoLoomThread()
+    {
+        int a = 10;
+        Loom.RunAsync(() =>
+        {
+            Thread.Sleep(1000);
+            Loom.QueueOnMainThread(() =>
+            {
+                GameObject go = new GameObject("asd");
+            });
+            GameObject oo = new GameObject("dasda");
+        });
+    }
+}
