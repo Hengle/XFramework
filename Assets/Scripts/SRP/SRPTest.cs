405f7b46992381ba1ce5af1d01bfcb8427070b06
xdedzl <32391533+xdedzl@users.noreply.github.com>
2019-01-16 17:44:47 +0800

    0

---
+++ Assets/Scripts/SRP/SRPTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SRPTest : MonoBehaviour
+{
+
+    private RenderTexture rt;
+    public Transform[] cubeTransforms;
+    public Mesh cubeMesh;
+    public Material pureColorMaterail;
+
+    void Start()
+    {
+        rt = new RenderTexture(Screen.width, Screen.height, 24);
+    }
+
+    // Update is called once per frame
+    void OnPostRender()
+    {
+        Camera cam = Camera.current;
+        Graphics.SetRenderTarget(rt);
+        GL.Clear(true, true, Color.gray);
+
+        // Start DrawCall
+        pureColorMaterail.color = new Color(0, .5f, 0.8f);
+        pureColorMaterail.SetPass(0);
+        foreach (var item in cubeTransforms)
+        {
+            Graphics.DrawMeshNow(cubeMesh, item.localToWorldMatrix);
+        }
+        // End DrawCall
+
+
+        Graphics.Blit(rt, cam.targetTexture);
+    }
+}
